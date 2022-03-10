027265a3182620dad006726b0e1f21a25759ad04
yumiko yamaguthi <fabulous1109@icloud.com>
2022-03-10 23:25:56 +0900

    [add]会員登録機能（見た目完了)

---
+++ layouts/auth.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <TheHeader />
+    <v-content style="height: auto;" class="pa-0 fill-height">
+      <v-container :class="{ 'pa-0': $vuetify.breakpoint.sm || $vuetify.breakpoint.xs }">
+        <nuxt />
+      </v-container>
+    </v-content>
+    <footer style>
+      <div class="pa-8 d-flex flex-warp justify-center fill-wdith">
+        <nuxt-link to="privacy" class="link-caption mr-8">プライバシーポリシー</nuxt-link>
+        <nuxt-link to="tos" class="link-caption mr-8">利用規約</nuxt-link>
+      </div>
+    </footer>
+  </v-app>
+</template>
+<script>
+import TheHeader from '~/components/shared/Header.vue'
+export default {
+  name: "auth",
+  components: {
+    TheHeader,
+  },
+}
+</script>
+
+<style lang="scss">
+.fill-width {
+  width: 100%;
+}
+
+.link-caption {
+  text-decoration: none;
+  color: #666 !important;
+  font-size: 0.75rem;
+}
+
+.separator {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  height: 0;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #fff;
+  position: relative;
+}
+
+.middle_separator {
+  position: absolute;
+  padding: 0 16px;
+  color: #ccc;
+  background: #fff;
+  font-size: 11px;
+  text-shadow: 0 1px 0 #fff;
+  text-transform: uppercase;
+  top: -7px;
+  left: 30%;
+}
+
+.middle_separator {
+  color: #777;
+  font-size: 13px;
+  top: -9px;
+  left: 41%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,62 @@
 <template>
   <main class="container">
-<div class="card" style="width: 20rem;">
-  <div class="card-body">
-    <h2 class="h2 text-center">Top page</h2>
-    <p class="card-text"></p>
-  </div>
-</div>
+    <div class="container-inner">
+      <v-card :tile="$vuetify.breakpoint.sm || $vuetify.breakpoint.xs" class="mx-auto fill-width" flat max-width="640">
+        <v-card-title class="text-center pa-8">
+          <h4 class="fill-width font-weight-bold" style="color:#385184;">会員登録</h4>
+        </v-card-title>
+        <div class="pt-6 text-center">ソーシャルアカウントで会員登録する</div>
+        <div class="px-6 py-8">
+          <div style="max-width:344px" class="mx-auto">
+            <div>
+              <v-btn class="fill-width text-capitalize caption" elevation="2" height="48px" outlined style="border-color:#979797;" rounded>
+                <img class="button-logo-img mr-4" src="https://madeby.google.com/static/images/google_g_logo.svg" style="height:24px;" />
+                Googleで登録
+              </v-btn>
+              <v-btn class="fill-width mt-5 text-capitalize caption" elevation="2" color="light-blue" dark depressed height="48px" rounded>
+                <img class="button-logo-img mr-4" src style="height:24px;" />
+                twitterで登録
+              </v-btn>
+              <v-btn class="fill-width mt-5 text-capitalize caption" elevation="2" color="#385184" dark depressed height="48px" rounded>
+                <img class="button-logo-img mr-4" src style="height:24px;" />
+                Facebookで登録
+              </v-btn>
+            </div>
+            <div class="separator separator_login_page">
+              <div class="middle_separator">または</div>
+            </div>
+            <div class="pt-6 text-center">メールアドレスとパスワードで登録する</div>
+            <div class="pt-6">
+              <div>
+                <v-text-field v-model="email" autofocus dense height="48px" outlined placeholder="メールアドレス"></v-text-field>
+                <v-text-field v-model="password" :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'" :type="passwordShow ? 'text' : 'password'" dense height="48px" name="input-password" outlined placeholder="パスワード" @click:append="passwordShow = !passwordShow"></v-text-field>
+              </div>
+            </div>
+            <div class="login-btn pb-8">
+              <v-btn class="fill-width caption" elevation="2" color="#DD693B" style="color:#fff;" depressed height="48px" rounded>会員登録</v-btn>
+            </div>
+            <v-divider></v-divider>
+            <div class="pt-8 pb-4">
+              <span>すでにアカウントをお持ちですか？</span>
+              <nuxt-link to="/login">ログインに移動</nuxt-link>
+            </div>
+          </div>
+        </div>
+      </v-card>
+    </div>
   </main>
 </template>
 
 <script>
 export default {
-  layout: "default",
+  layout: "auth",
   components: {
   }
 }
 </script>
+
+<style lang="scss">
+.container-inner {
+  padding-top: 100px;
+}
+</style>
